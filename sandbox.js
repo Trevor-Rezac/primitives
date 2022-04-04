a16c5ee39bebe77444898d2d06c1e99ccb89954b
TrevorRezac <trev.rezac@gmail.com>
2022-04-04 10:56:02 -0700

    oddishOrEvenish returns oddish or evenish correctly

--- sandbox.js
+++ sandbox.js
@@ -10,3 +10,22 @@ function reverseWords(sentence) {
 }
 
 console.log(reverseWords('alchemy rocks gold'));
+
+//input: 121
+//output: 'Evenish'
+//because: 1 + 2 + 1 = 4
+
+function oddishOrEvenish(number) {
+  const digits = number.toString().split('');
+  console.log('digits: ', digits);
+  const numArr = digits.map((digit) => +digit);
+  const initialValue = 0;
+  const sum = numArr.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  const evenOrOdd = sum % 2 === 0 ? 'evenish' : 'oddish';
+  return evenOrOdd;
+}
+
+console.log(oddishOrEvenish(41));
