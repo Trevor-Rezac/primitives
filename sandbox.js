33b2291b5365dc8b127ed529dcd88b463baf1f4f
TrevorRezac <trev.rezac@gmail.com>
2022-04-04 12:28:30 -0700

    fizzBuzz function works properly

--- sandbox.js
+++ sandbox.js
@@ -17,15 +17,48 @@ console.log(reverseWords('alchemy rocks gold'));
 
 function oddishOrEvenish(number) {
   const digits = number.toString().split('');
-  console.log('digits: ', digits);
+
   const numArr = digits.map((digit) => +digit);
   const initialValue = 0;
   const sum = numArr.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     initialValue
   );
-  const evenOrOdd = sum % 2 === 0 ? 'evenish' : 'oddish';
+  const evenOrOdd = sum % 2 ? 'oddish' : 'evenish';
   return evenOrOdd;
 }
 
 console.log(oddishOrEvenish(41));
+console.log(oddishOrEvenish(121));
+
+//input: 16
+// expected output: [
+//   1, 2, 'Fizz', 4, 'Buzz',
+//   'Fizz', 7, 8, 'Fizz', 'Buzz',
+//   11, 'Fizz', 13, 14, 'FizzBuzz',
+//   16
+// ]
+//output returns "fizz" for multiples of 3 and "buzz" for multiples of 5
+//output return "fizzbuzz" for numbers which are multiples of both 3 and 5
+
+function fizzBuzz(number) {
+  const numArr = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+      numArr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+      numArr.push('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+      numArr.push('Buzz');
+    } else {
+      console.log(i);
+      numArr.push(i);
+    }
+  }
+  return numArr;
+}
+
+console.log('results: ', fizzBuzz(16));
